697d019c0a2aeefa0832524794196bcee16246fd
me990928 <me990928@gmail.com>
2025-04-29 16:00:43 +0900

    買い物リスト画面の追加

--- KaimonoBukka/Kaimono/KaimonoView.swift
+++ KaimonoBukka/Kaimono/KaimonoView.swift
@@ -9,7 +9,47 @@ import SwiftUI
 
 struct KaimonoView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            ScrollView {
+                LazyVStack {
+                    NavigationLink {
+                        
+                    } label: {
+                        InformationCard(cardColor: Color(.systemGray2)) {
+                            ZStack {
+                                VStack(alignment: .leading) {
+                                    Text("カレーを買いに行く今日もお聞き機佐久間さん").font(.headline).padding(.top)
+                                    Divider()
+                                    Text("店名: OK")
+                                    Text("予定日: 2025年4月29日")
+                                }.padding()
+                                VStack{
+                                    HStack{
+                                        Spacer()
+                                        Menu {
+                                            Text("Delete")
+                                        } label: {
+                                            Image(systemName: "ellipsis")
+                                        }
+                                    }
+                                    Spacer()
+                                }.padding()
+                            }
+                        }.padding()
+                    }
+
+                }
+            }.toolbar {
+                ToolbarItem {
+                    Button {
+                        // 作成フォームへ
+                    } label: {
+                       Image(systemName: "plus")
+                    }
+
+                }
+            }
+        }
     }
 }
 
---
+++ KaimonoBukka/Kaimono/Pages/KaimonoDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  KaimonoDetailView.swift
+//  KaimonoBukka
+//
+//  Created by 広瀬友哉 on 2025/04/29.
+//
+
+import SwiftUI
+
+struct KaimonoDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    KaimonoDetailView()
+}

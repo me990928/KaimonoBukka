aa394893efdbc5caeb76bd091dd2c2e930225bd7
me990928 <me990928@gmail.com>
2025-04-29 17:29:41 +0900

    編集フォームの追加

--- KaimonoBukka/Kaimono/Pages/KaimonoEditView.swift
+++ KaimonoBukka/Kaimono/Pages/KaimonoEditView.swift
@@ -8,11 +8,50 @@
 import SwiftUI
 
 struct KaimonoEditView: View {
+    
+    let editorTitle: String
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            VStack(alignment: .leading) {
+                // 編集フォーム
+                HStack {
+                    Text(editorTitle).font(.largeTitle).bold()
+                    Spacer()
+                }
+                
+                HStack {
+                    Text("タイトル: ")
+                    TextField("", text: .constant("")).textFieldStyle(.roundedBorder)
+                    Spacer()
+                }
+                
+                HStack {
+                    Text("店名: ")
+                    TextField("", text: .constant("")).textFieldStyle(.roundedBorder)
+                    Spacer()
+                }
+                
+                HStack {
+                    Text("予定日: ")
+                    DatePicker("", selection: .constant(Date())).datePickerStyle(.compact)
+                    Spacer()
+                }
+                
+                // アップデートで予定日を通知orリマインダーやカレンダーに追加
+                
+                Spacer()
+            }.padding()
+        }.safeAreaInset(edge: .bottom) {
+            Button {
+                
+            } label: {
+                Text("保存")
+            }.buttonStyle(.borderedProminent).padding()
+        }
     }
 }
 
 #Preview {
-    KaimonoEditView()
+    KaimonoEditView(editorTitle: "編集画面")
 }
